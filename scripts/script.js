98042348d929aef7328b62c49e26fb8facc641e6
enoripl <tomek00@gmail.com>
2021-10-01 23:54:20 +0200

    prettified

--- scripts/script.js
+++ scripts/script.js
@@ -20,21 +20,24 @@ function draw() {
 }
 
 draw();
-restartButton()
+restartButton();
 
 function drawNew() {
   const myNode = document.getElementById("boardContainer");
-  const userInput = prompt(`How big of a rectangle do you need chief? \nEnter a number (max. 100)`);
+  const userInput = prompt(
+    `How big of a rectangle do you need chief? \nEnter a number (max. 100)`
+  );
   if (userInput >= 1 && userInput <= 100) {
-    myNode.innerHTML = '';
-  for (let i = 0; i < userInput * userInput; i++) {
-    const boardContainer = document.querySelector("#boardContainer");
-    const s = (100 / userInput);
-    const divs = document.createElement("div");
-    divs.classList.add("pixels");
-    divs.style.cssText = "width: " + s + "%; height: " + s + "%";
-    boardContainer.appendChild(divs);
-  }} else if (userInput === null) {
+    myNode.innerHTML = "";
+    for (let i = 0; i < userInput * userInput; i++) {
+      const boardContainer = document.querySelector("#boardContainer");
+      const s = 100 / userInput;
+      const divs = document.createElement("div");
+      divs.classList.add("pixels");
+      divs.style.cssText = "width: " + s + "%; height: " + s + "%";
+      boardContainer.appendChild(divs);
+    }
+  } else if (userInput === null) {
     return;
   } else {
     alert("Please enter a number between 1 and 100");
@@ -44,9 +47,8 @@ function drawNew() {
 }
 
 function restartButton() {
-  const button = document.querySelector("#restartButton")
-  button.addEventListener('click', () => {
-    drawNew()
-  } );
+  const button = document.querySelector("#restartButton");
+  button.addEventListener("click", () => {
+    drawNew();
+  });
 }
-
